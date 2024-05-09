32e0642b27cf172f6dd019d78d95626f109bb3b6
adrian-kriegel <ak@adriankriegel.com>
2024-05-09 14:26:05 +0200

    added cmake option ACADOS_CPP_BUILD_TESTS

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 #!/bin/env bash
 
-cd build
-cmake .. 
-make testACADOSCpp -j4
-cd ..
+cmake -S . -B build -DACADOS_CPP_BUILD_TESTS=ON
+cmake --build build 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCRIPT_DIR/test/lib
