22c88822facf7cc5b78f78ecc1c4c19fb6341abf
Adrian Kriegel <23387365+adrian-kriegel@users.noreply.github.com>
2024-11-09 14:04:35 +0100

    comment

--- test.sh
+++ test.sh
@@ -3,9 +3,8 @@
 cmake -S . -B build -DACADOS_CPP_BUILD_TESTS=ON
 cmake --build build 
 
+# Add test lib dir to LD_LIBRARY_PATH
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCRIPT_DIR/test/lib
-
-echo $LD_LIBRARY_PATH
 
 ./build/test/testACADOSCpp
